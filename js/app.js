078ba825f2e6002ee011b1a2364199f93d79a200
Chris.Z <zlvsky@icloud.com>
2020-05-07 18:29:21 +0200

    opisy

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@
     var router = new Router([
       new Route('home', 'home.html', true),
       new Route('contact', 'contact.html'),
-      new Route('about', 'about.html')
+      new Route('about', 'about.html'),
+      new Route('zamowienia', 'zamowienia.html'),
+      new Route('informacje', 'informacje.html')
     ]);
   }
   init();
